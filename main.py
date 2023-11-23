61e8428cfb9abf504da6795176373b056c757c55
ismardn <ismael.radouane@efrei.net>
2023-11-23 23:05:36 +0100

    Projet

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import tkinter
-import tkinter.messagebox
 import os
 import math
 
@@ -37,16 +35,6 @@ def recup_noms_presidents(nom_fichier_presidents, nom_repertoire):
         if nom_president not in noms_presidents:
             noms_presidents.append(nom_president)
 
-    with open(nom_fichier_presidents, "r") as fichier:
-        contenu_fichier = ""
-        for ligne in fichier:
-            contenu_fichier += ligne
-
-    with open(nom_fichier_presidents, "a") as fichier:  # Ajout des noms des présidents dans un fichier
-        for nom_president in noms_presidents:
-            if nom_president not in contenu_fichier:
-                fichier.write(nom_president + ":\n")
-
     return noms_presidents
 
 
@@ -56,7 +44,7 @@ def recup_prenoms_presidents(nom_fichier_presidents):
     with open(nom_fichier_presidents, "r") as fichier:
         for ligne in fichier:
             ligne_split = ligne.split(":")
-            prenoms_presidents.append(ligne_split[1])
+            prenoms_presidents.append(ligne_split[1].rstrip())
 
     return prenoms_presidents
 
@@ -332,7 +320,7 @@ def mot_enonce_president(nom_repertoire, mot_recherche):
     return [nom_president for nom_president in dict_pres_mot], liste_pres_max
 
 
-def premier_president_mot(nom_repertoire, liste_mot_recherche):
+def premier_president_mot(nom_repertoire, mot_recherche):
     noms_fichiers = liste_fichiers(nom_repertoire, "txt")
 
     dict_pres_mot = {}
@@ -348,21 +336,24 @@ def premier_president_mot(nom_repertoire, liste_mot_recherche):
             nom_president = retirer_caracteres_nom_fichier(nom_fichier)
 
             for indice_mot in range(len(contenu_fichier_split)):
-                if contenu_fichier_split[indice_mot] in liste_mot_recherche:
+                if contenu_fichier_split[indice_mot] == mot_recherche:
                     if nom_president in dict_pres_mot:
                         if dict_pres_mot[nom_president] < indice_mot:
                             dict_pres_mot[nom_president] = indice_mot
                     else:
                         dict_pres_mot[nom_president] = indice_mot
 
-    premier_pres = list(dict_pres_mot.keys())[0]
-    liste_pres = [dict_pres_mot[premier_pres], premier_pres]
+    if dict_pres_mot != {}:
+        premier_pres = list(dict_pres_mot.keys())[0]
+        liste_pres = [dict_pres_mot[premier_pres], premier_pres]
 
-    for nom_president in dict_pres_mot:
-        if dict_pres_mot[nom_president] < liste_pres[0]:
-            liste_pres = [dict_pres_mot[nom_president], nom_president]
+        for nom_president in dict_pres_mot:
+            if dict_pres_mot[nom_president] < liste_pres[0]:
+                liste_pres = [dict_pres_mot[nom_president], nom_president]
 
-    return liste_pres[1]
+        return liste_pres[1]
+    else:
+        return "Aucun président n'a utilisé ce mot"
 
 
 ########################################################################################################################
@@ -370,45 +361,127 @@ def premier_president_mot(nom_repertoire, liste_mot_recherche):
 ########################################################################################################################
 
 
-def fenetre_aide():
-    with open("README.txt", "r") as fichier:
-        contenu_fichier = ""
-        for ligne in fichier:
-            contenu_fichier += ligne
-    tkinter.messagebox.showinfo(title="Aide", message=contenu_fichier.encode())
-
-
-# test
-
 def main():
     NOM_REPERTOIRE_DISCOURS = "speeches"
     NOM_REPERTOIRE_NETTOYE = "cleaned"
 
     NOM_FICHIER_PRESIDENTS = "presidents.txt"  # Noms et prénoms stockés dans ce fichier
 
-    fenetre = tkinter.Tk()
+    noms_presidents = recup_noms_presidents(NOM_FICHIER_PRESIDENTS, NOM_REPERTOIRE_DISCOURS)
 
-    fenetre.geometry("600x400")
-    fenetre.configure(background="white")
-    fenetre.resizable(width=False, height=False)
+    nettoyage_complet_fichiers(NOM_REPERTOIRE_DISCOURS, NOM_REPERTOIRE_NETTOYE)
 
-    aide = tkinter.Menu(fenetre)
-    aide.add_command(label="Aide", command=fenetre_aide)
-    fenetre.config(menu=aide)
+    matrice_tf_idf = creation_matrice(NOM_REPERTOIRE_NETTOYE)
 
-    # noms_presidents = recup_noms_presidents(nom_fichier_presidents, nom_repertoire_discours)
-    #
-    # print(recup_noms_presidents(nom_fichier_presidents, nom_repertoire_discours))
-    #
-    # # Ajout des prénoms des présidents manuellement dans le fichier "presidents.txt"
-    #
-    # prenoms_presidents = recup_prenoms_presidents(nom_fichier_presidents)
-    #
-    # nettoyage_complet_fichiers(nom_repertoire_discours, nom_repertoire_nettoye)
-    #
-    # print(creation_matrice(nom_repertoire_nettoye))
+    print("\nBienvenue sur le ChatBot de Clément PINTO RIBEIRO et de Ismaël RADOUANE.\n")
 
-    fenetre.mainloop()
+    demander_numero = True
+
+    while demander_numero:
+        print("\nPour accéder aux différents menus, veuillez entrer le numéro associé :\n\n"
+              "1. Lire la notice d'utilisation\n"
+              "2. Accéder aux noms des présidents\n"
+              "3. Changer/Accéder aux prénoms des présidents\n"
+              "4. Afficher la liste des mots les moins importants dans le corpus de documents\n"
+              "5. Afficher la liste de mot ayant le score TF-IDF le plus élevé\n"
+              "6. Afficher la liste de mot les plus répétés par le président choisi\n"
+              "7. Afficher les noms des présidents qui ont parlé du mot choisi\n"
+              "8. Afficher le premier président à utiliser le mot choisi\n"
+              "9. Accéder à la matrice TF-IDF\n"
+              "Ou entrez \"q\" pour quitter\n")
+
+        reponse_accepte = False
+        while not reponse_accepte:
+            reponse_utilisateur = input("Entrez le numéro choisi : ")
+            if reponse_utilisateur in [str(nombre) for nombre in range(1, 10)] + ["q"]:
+                reponse_accepte = True
+            else:
+                print("\nErreur : Veuillez entrer un nombre entier entre 1 et 9\n")
+
+        print()
+        if reponse_utilisateur == "1":
+            with open("README.txt", "r") as fichier:
+                contenu_fichier = ""
+                for ligne in fichier:
+                    contenu_fichier += ligne
+            return contenu_fichier
+
+        elif reponse_utilisateur == "2":
+            print("Les noms des présidents présents dans le corpus de documents sont :")
+            for nom_president in noms_presidents:
+                print("- " + nom_president)
+
+        elif reponse_utilisateur == "3":
+            prenoms_presidents = recup_prenoms_presidents(NOM_FICHIER_PRESIDENTS)
+            if prenoms_presidents[-1] == "":
+                input_utilisateur = input("Certains présidents n'ont pas de prénoms associés à leur nom."
+                      "Entrez \"o\" pour ajouter les prénoms : ")
+                if input_utilisateur == "o":
+                    contenu_fichier = ""
+                    for nom_president in noms_presidents:
+                        print("Entrez le prénom que vous souhaitez associer à", nom_president, " : ", end="")
+                        contenu_fichier += nom_president + ":" + input() + "\n"
+                    with open(NOM_FICHIER_PRESIDENTS, "w") as fichier:
+                        fichier.write(contenu_fichier)
+            else:
+                print("Les prénoms associés aux présidents sont :")
+                with open(NOM_FICHIER_PRESIDENTS, "r") as fichier:
+                    for ligne in fichier:
+                        ligne_split = ligne.split(":")
+                        print("- " + ligne_split[0] + " : " + ligne_split[1])
+
+        elif reponse_utilisateur == "4":
+            print("Les mots les moins importants dans le corpus de documents sont :")
+            for mot in tf_idf_nul(matrice_tf_idf):
+                print("- " + mot)
+
+        elif reponse_utilisateur == "5":
+            print("Les mots les plus importants dans le corpus de documents sont :")
+            for mot in tf_idf_max(matrice_tf_idf):
+                print("- " + mot)
+
+        elif reponse_utilisateur == "6":
+            reponse_valide = False
+
+            while not reponse_valide:
+                input_president = input("Entrez le nom du président choisi : ")
+
+                if input_president in noms_presidents:
+                    reponse_valide = True
+                    liste_mot_max_pres = mot_max_president(NOM_REPERTOIRE_NETTOYE, input_president)
+                    print("\nLes mots les plus répétés par le président", input_president, "sont :")
+                    for mot in liste_mot_max_pres:
+                        print("- " + mot)
+                else:
+                    print("\nErreur : Veuillez entrer un nom de président présent dans le corpus de documents (", end="")
+                    for indice_nom in range(len(noms_presidents) - 1):
+                        print(noms_presidents[indice_nom], end=", ")
+                    print(noms_presidents[-1] + ")\n")
+
+        elif reponse_utilisateur == "7":
+            input_mot = input("Entrez le mot recherché : ")
+            return_mot_enonce_president = mot_enonce_president(NOM_REPERTOIRE_NETTOYE, input_mot)
+
+            print('\nLes présidents qui ont énoncé le mot "' + input_mot + '" sont :')
+            for nom_president in return_mot_enonce_president[0]:
+                print("- " + nom_president)
+
+            print("\nLes présidents qui l'ont énoncé le plus de fois sont :")
+            for nom_president in return_mot_enonce_president[1]:
+                print("- " + nom_president)
+
+        elif reponse_utilisateur == "8":
+            input_mot = input("Entrez le mot recherché : ")
+            print('\nLe premier président à utiliser le mot "' + input_mot + '" est :',
+                  premier_president_mot(NOM_REPERTOIRE_NETTOYE, input_mot))
+
+        elif reponse_utilisateur == "9":
+            pass
+
+        elif reponse_utilisateur == "q":
+            demander_numero = False
+
+        print()
 
 
 main()
