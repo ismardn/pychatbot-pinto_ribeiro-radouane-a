192f00b28bb8c59cd8561ef917c8845a41fc4751
ismardn <ismael.radouane@efrei.net>
2023-11-26 13:16:17 +0100

    Commentaires

--- fonctions_matrice.py
+++ fonctions_matrice.py
@@ -2,7 +2,8 @@ import os
 import math
 
 
-def liste_fichiers(repertoire, extension):   #Création d'une liste comprenant tout les noms des fichers. txt présent dans le corpus#
+# Création d'une liste comprenant tout les noms des fichers. txt présent dans le corpus
+def liste_fichiers(repertoire, extension):
     noms_fichiers = []
     for nom_fichier in os.listdir(repertoire):
         if nom_fichier.endswith(extension):
@@ -22,7 +23,8 @@ def retirer_caracteres_nom_fichier(nom_fichier):
     return nom_president
 
 
-def recup_noms_presidents(nom_repertoire):    # Fonction pour remettre au propre la liste du nom des présidents en retirant les numéruos et les doublons
+# Fonction pour remettre au propre la liste du nom des présidents en retirant les numéruos et les doublons
+def recup_noms_presidents(nom_repertoire):
     noms_fichiers = liste_fichiers(nom_repertoire, "txt")
 
     noms_presidents_temp = []
@@ -38,12 +40,13 @@ def recup_noms_presidents(nom_repertoire):    # Fonction pour remettre au propre
     return noms_presidents
 
 
-def en_minuscule(chaine):     #Transformation des textes des fichiers: mise en forme des textes en mettant tout en minuscule.
+# Transformation des textes des fichiers: mise en forme des textes en mettant tout en minuscule
+def en_minuscule(chaine):
     nouvelle_chaine = ""
 
     for caractere in chaine:
-        if ord("A") <= ord(caractere) <= ord('Z'): #Utilisation du code ASCII
-            nouvelle_chaine += chr(ord(caractere) + ord("a") - ord("A"))
+        if ord("A") <= ord(caractere) <= ord('Z'):  # Si le code ASCII du caractère appartient aux majuscules
+            nouvelle_chaine += chr(ord(caractere) + ord("a") - ord("A"))  # On le transforme en minuscule
         else:
             nouvelle_chaine += caractere
 
@@ -59,9 +62,10 @@ def creer_fichiers_minuscule(nom_repertoire_discours, nom_repertoire_nettoye):
             fichier_nettoye.write(en_minuscule(fichier_ancien.read()))
 
 
-def suppression_caracteres_speciaux(chaine):        #Remise en forme des textes en enlevant les caractères spéciaux ainsi que les numéros.
+# Remise en forme des textes en enlevant les caractères spéciaux ainsi que les numéros.
+def suppression_caracteres_speciaux(chaine):
     caracteres_speciaux = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-                           ',', '-', "'", '.', '!', '?', '_', ':', '\n', '"', ';', '`']
+                           ',', '-', "'", '.', '!', '?', '_', ':', '\n', '"', ';', '`']  # Caractères supprimés
 
     nouvelle_chaine = ""
 
@@ -91,11 +95,11 @@ def nettoyage_complet_fichiers(nom_repertoire_discours, nom_repertoire_nettoye):
             fichier.write(suppression_caracteres_speciaux(ancien_contenu))
 
 
-def calcul_tf(chaine):      #Fonction pour calculer le TF
+def calcul_tf(chaine):  # Fonction pour calculer le TF
     liste_mots = chaine.split()
     dictionnaire = {}
 
-    for mot in liste_mots:      #Mise en place d'un dictionnaire associant à chaque mot son score
+    for mot in liste_mots:  # Mise en place d'un dictionnaire associant à chaque mot son score
         if mot not in dictionnaire:
             dictionnaire[mot] = 1
         else:
@@ -104,7 +108,7 @@ def calcul_tf(chaine):      #Fonction pour calculer le TF
     return dictionnaire
 
 
-def calcul_tf_total(nom_repertoire):        # Application de la fonction TF à tout les fichiers.
+def calcul_tf_total(nom_repertoire):  # Application de la fonction TF à tout les fichiers
     liste_tf = []
 
     noms_fichiers = liste_fichiers(nom_repertoire, "txt")
@@ -117,7 +121,7 @@ def calcul_tf_total(nom_repertoire):        # Application de la fonction TF à t
     return liste_tf
 
 
-def calcul_idf_total(nom_repertoire):       #Fonction permettant de calculer le score IDF de chaque mot
+def calcul_idf_total(nom_repertoire):  # Fonction permettant de calculer le score IDF de chaque mot
     noms_fichiers = liste_fichiers(nom_repertoire, "txt")
 
     contenu_fichiers_liste = []
@@ -136,7 +140,7 @@ def calcul_idf_total(nom_repertoire):       #Fonction permettant de calculer le 
         if mot not in liste_mots:
             liste_mots.append(mot)
 
-    dictionnaire = {}       #Réutilisation d'un dictionnaire ici aussi pour associer à chaque mot son score
+    dictionnaire = {}  # Réutilisation d'un dictionnaire ici aussi pour associer à chaque mot son score
 
     for mot in liste_mots:
 
@@ -150,7 +154,8 @@ def calcul_idf_total(nom_repertoire):       #Fonction permettant de calculer le 
     return dictionnaire
 
 
-def transposee_matrice(matrice):    #Fonction pour réalisé la transposée d'une matrice qui consiste à inverse les lignes et les colonnes de celle ci
+# Fonction pour réalisé la transposée d'une matrice qui consiste à inverse les lignes et les colonnes de celle ci
+def transposee_matrice(matrice):
     nouvelle_matrice = []
 
     for indice_colonne in range(len(matrice[0])):
@@ -158,14 +163,15 @@ def transposee_matrice(matrice):    #Fonction pour réalisé la transposée d'un
         ligne_nouvelle_matrice = []
 
         for indice_ligne in range(len(matrice)):
-            ligne_nouvelle_matrice.append(matrice[indice_ligne][indice_colonne]) # Changement réalisé ici
+            ligne_nouvelle_matrice.append(matrice[indice_ligne][indice_colonne])  # Changement réalisé ici
 
         nouvelle_matrice.append(ligne_nouvelle_matrice)
 
     return nouvelle_matrice
 
 
-def creation_matrice(nom_repertoire):       #Création de notre matrcice contenant le noms des fichiers + tous les mots présents dans les fichiers et leurs score TF-IDF associé.
+# Création de notre matrcice associant un score TF-IDF pour chaque noms de fichiers en fonctions des mots
+def creation_matrice(nom_repertoire):
     noms_fichiers = liste_fichiers(nom_repertoire, "txt")
 
     valeur_idf_fichier = calcul_idf_total(nom_repertoire)
@@ -181,11 +187,12 @@ def creation_matrice(nom_repertoire):       #Création de notre matrcice contena
         with open(nom_repertoire + "/" + noms_fichiers[indice_fichier], "r", encoding="utf-8") as fichier:
             contenu_fichier_split = fichier.read().split()
 
-            valeur_tf_fichier = calcul_tf_total(nom_repertoire)[indice_fichier]     #Utilisation de la fonction TF
+            valeur_tf_fichier = calcul_tf_total(nom_repertoire)[indice_fichier]  # Utilisation de la fonction TF
 
             for mot in liste_mots:
                 if mot in contenu_fichier_split:
-                    tf_idf_mot.append(valeur_idf_fichier[mot] * valeur_tf_fichier[mot])         #Mise en place de la fonction TF-IDF
+                    # Mise en place de la fonction TF-IDF
+                    tf_idf_mot.append(valeur_idf_fichier[mot] * valeur_tf_fichier[mot])
                 else:
                     tf_idf_mot.append(0.)
 
@@ -194,7 +201,7 @@ def creation_matrice(nom_repertoire):       #Création de notre matrcice contena
     return noms_fichiers, liste_mots, transposee_matrice(matrice)
 
 
-def tf_idf_nul(return_matrice):       #Fonction renvoyant les mots avec un score TF-IDF nul
+def tf_idf_nul(return_matrice):  # Fonction renvoyant les mots avec un score TF-IDF nul
     noms_fichiers, liste_mots, matrice = return_matrice
 
     moyenne_tf_idf_mots = []
@@ -208,13 +215,14 @@ def tf_idf_nul(return_matrice):       #Fonction renvoyant les mots avec un score
     liste_valeurs_min = []
 
     for indice_valeur_min in range(len(moyenne_tf_idf_mots)):
-        if moyenne_tf_idf_mots[indice_valeur_min] == 0.:        #Comparaison du score TF-IDF à 0, pour trouver les mots avec un score nul
+        # Comparaison du score TF-IDF à 0, pour trouver les mots avec un score nul
+        if moyenne_tf_idf_mots[indice_valeur_min] == 0.:
             liste_valeurs_min.append(liste_mots[indice_valeur_min])
 
     return liste_valeurs_min
 
 
-def tf_idf_max(return_matrice):             #Fonction renvoyant les mots avec le score TF-IDF le plus élevé.
+def tf_idf_max(return_matrice):  # Fonction renvoyant les mots avec le score TF-IDF le plus élevé.
     noms_fichiers, liste_mots, matrice = return_matrice
 
     moyenne_tf_idf_mots = []
@@ -228,16 +236,17 @@ def tf_idf_max(return_matrice):             #Fonction renvoyant les mots avec le
     liste_valeurs_max = [-1, []]
 
     for valeur in moyenne_tf_idf_mots:
-        if valeur > liste_valeurs_max[0]:          #Mise en place d'une valeur max
+        if valeur > liste_valeurs_max[0]:  # Mise en place d'une valeur max
             liste_valeurs_max = [valeur, []]
             for indice_valeur_max in range(len(moyenne_tf_idf_mots)):
-                if moyenne_tf_idf_mots[indice_valeur_max] == valeur:            #Comparaison du score de chaque mot avec cette valeur max.
+                # Comparaison du score de chaque mot avec cette valeur max.
+                if moyenne_tf_idf_mots[indice_valeur_max] == valeur:
                     liste_valeurs_max[1].append(liste_mots[indice_valeur_max])
 
     return liste_valeurs_max[1]
 
 
-def mot_max_president(nom_repertoire, nom_president):       #Fonction renvoyant le mot le plus utilisé par un président
+def mot_max_president(nom_repertoire, nom_president):  # Fonction renvoyant le mot le plus utilisé par un président
     noms_fichiers = liste_fichiers(nom_repertoire, "txt")
 
     contenu_fichiers = ""
@@ -251,7 +260,7 @@ def mot_max_president(nom_repertoire, nom_president):       #Fonction renvoyant 
 
     tf_contenu_fichiers = calcul_tf(contenu_fichiers)
 
-    for mot in tf_contenu_fichiers:             #Reprise de la même base de code que pour la fonction TF-IDF max.
+    for mot in tf_contenu_fichiers:  # Reprise de la même base de code que pour la fonction TF-IDF max.
         if tf_contenu_fichiers[mot] > liste_valeurs_max[0]:
             liste_valeurs_max = [tf_contenu_fichiers[mot], []]
             for mot_max in tf_contenu_fichiers:
@@ -292,7 +301,8 @@ def mot_enonce_president(nom_repertoire, mot_recherche):
     return [nom_president for nom_president in dict_pres_mot], liste_pres_max
 
 
-def premier_president_mot(nom_repertoire, mot_recherche):  #Fonction permettrant de trouver le ppremier président à avoir parler d'un mot ou d'un sujet
+# Fonction permettrant de trouver le ppremier président à avoir parler d'un mot ou d'un sujet
+def premier_president_mot(nom_repertoire, mot_recherche):
     noms_fichiers = liste_fichiers(nom_repertoire, "txt")
 
     dict_pres_mot = {}
@@ -322,7 +332,8 @@ def premier_president_mot(nom_repertoire, mot_recherche):  #Fonction permettrant
         return liste_pres[1]
 
 
-def mots_tous_presidents(return_matrice, nom_repertoire_nettoye):       #Fonction permettant de renvoyer les mots que tout les président on évoqué au moins une fois.
+# Fonction permettant de renvoyer les mots que tout les président on évoqué au moins une fois.
+def mots_tous_presidents(return_matrice, nom_repertoire_nettoye):
     noms_fichiers, liste_mots, matrice = return_matrice
 
     liste_mots_non_important = tf_idf_nul(return_matrice)
@@ -336,7 +347,8 @@ def mots_tous_presidents(return_matrice, nom_repertoire_nettoye):       #Fonctio
     liste_mots_tous_president = []
 
     for mot in liste_mots:
-        mot_in_fichiers = [mot in liste_contenu_fichier_split[indice_fichier] for indice_fichier in range(len(noms_fichiers))]
+        mot_in_fichiers = [mot in liste_contenu_fichier_split[indice_fichier]
+                           for indice_fichier in range(len(noms_fichiers))]
 
         dict_pres = {}
         for nom_president in recup_noms_presidents(nom_repertoire_nettoye):
--- main.py
+++ main.py
@@ -2,19 +2,23 @@ import os
 import fonctions_matrice as fct_mat
 
 
+# Fonction permettant d'afficher le contenu du fichier "README.txt"
 def fonctionnalite_1():
     with open("README.txt", "r", encoding="utf-8") as fichier:
+        # Ouverture du fichier avec l'encodage "utf-8" afin de conserver les accents
         print(fichier.read())
 
 
+# Afficher l'ensemble des présidents présents dans le corpus de documents sous la forme de liste
 def fonctionnalite_2(noms_presidents):
     print("Les noms des présidents présents dans le corpus de documents sont :")
     for nom_president in noms_presidents:
         print("- " + nom_president)
 
 
+# Afficher l'ensemble des prénoms des présidents et/ou les modifier
 def fonctionnalite_3(nom_fichier_presidents, noms_presidents):
-    if not os.path.isfile(nom_fichier_presidents):
+    if not os.path.isfile(nom_fichier_presidents):  # Si le fichier "presidents.txt" n'existe pas, le créer
         open(nom_fichier_presidents, "x")
 
     with open(nom_fichier_presidents, "r", encoding="utf-8") as fichier:
@@ -22,12 +26,14 @@ def fonctionnalite_3(nom_fichier_presidents, noms_presidents):
     tous_prenoms = True
     for nom_president in noms_presidents:
         if nom_president not in contenu_fichier:
+            # Si un président n'est pas dans le fichier "presidents.txt", alors le contenu du fichier doit être modifié
             tous_prenoms = False
 
-    def modif_prenoms():
+    def modif_prenoms():  # Fonction interne qui permet d'associer des prénoms aux noms des présidents
         contenu_fichier = ""
         for nom_president in noms_presidents:
             print("Entrez le prénom que vous souhaitez associer à", nom_president, ": ", end="")
+            # On stocke les prénoms des présidents sous la forme "nom1:prénom1\nnom2:prénom2\n..."
             contenu_fichier += nom_president + ":" + input() + "\n"
         with open(nom_fichier_presidents, "w", encoding="utf-8") as fichier:
             fichier.write(contenu_fichier)
@@ -37,38 +43,38 @@ def fonctionnalite_3(nom_fichier_presidents, noms_presidents):
                                   "Entrez \"o\" pour ajouter les prénoms : ")
         if input_utilisateur == "o":
             modif_prenoms()
-    else:
+    else:  # Si tous les présidents ont un prénom associé, alors on se contente de les afficher
         print("Les prénoms associés aux présidents sont :")
         with open(nom_fichier_presidents, "r", encoding="utf-8") as fichier:
             for ligne in fichier:
-                ligne_split = ligne.split(":")
+                ligne_split = ligne.split(":")  # On récupère les noms/prénoms sous forme de listes
                 print("- " + ligne_split[0] + " : " + ligne_split[1], end="")
         print()
         input_utilisateur = input("Souhaitez vous modifier les prénoms associés aux noms des présidents ?\n"
                                   "Entrez \"o\" pour modifier les prénoms : ")
-        if input_utilisateur == "o":
+        if input_utilisateur == "o":  # On propose à l'utilisateur de modifier les prénoms
             modif_prenoms()
 
 
-def fonctionnalite_4(return_matrice_tf_idf):
+def fonctionnalite_4(return_matrice_tf_idf):  # Fonctionnalité n°4 (même principe que la fonctionnalité n°2)
     print("Les mots les moins importants dans le corpus de documents sont :")
     for mot in fct_mat.tf_idf_nul(return_matrice_tf_idf):
         print("- " + mot)
 
 
-def fonctionnalite_5(return_matrice_tf_idf):
+def fonctionnalite_5(return_matrice_tf_idf):  # Fonctionnalité n°5 (même principe que la fonctionnalité n°2)
     print("Les mots ayant le score TF-IDF le plus élevé sont :")
     for mot in fct_mat.tf_idf_max(return_matrice_tf_idf):
         print("- " + mot)
 
 
-def fonctionnalite_6(noms_presidents, nom_repertoire_nettoye):
+def fonctionnalite_6(noms_presidents, nom_repertoire_nettoye):  # Fonctionnalité n°6
     reponse_valide = False
 
-    while not reponse_valide:
+    while not reponse_valide:  # Tant que l'utilisateur n'entre pas un nom de président présent dans le corpus
         input_president = input("Entrez le nom du président choisi : ")
 
-        if input_president in noms_presidents:
+        if input_president in noms_presidents:  # Si le président existe, on affiche ses mots les plus répétés
             reponse_valide = True
             liste_mot_max_pres = fct_mat.mot_max_president(nom_repertoire_nettoye, input_president)
             print("\nLes mots les plus répétés par le président", input_president, "sont :")
@@ -82,7 +88,7 @@ def fonctionnalite_6(noms_presidents, nom_repertoire_nettoye):
             print(noms_presidents[-1] + ")\n")
 
 
-def fonctionnalite_7(nom_repertoire_nettoye):
+def fonctionnalite_7(nom_repertoire_nettoye):  # Fonctionnalité n°7
     input_mot = input("Entrez le mot recherché : ")
     return_mot_enonce_president = fct_mat.mot_enonce_president(nom_repertoire_nettoye, input_mot)
 
@@ -98,21 +104,23 @@ def fonctionnalite_7(nom_repertoire_nettoye):
             print("- " + nom_president)
 
 
-def fonctionnalite_8(nom_repertoire_nettoye):
+def fonctionnalite_8(nom_repertoire_nettoye):  # Fonctionnalité n°8
     input_mot = input("Entrez le mot recherché : ")
     print('\nLe premier président à utiliser le mot "' + input_mot + '" est :',
           fct_mat.premier_president_mot(nom_repertoire_nettoye, input_mot))
 
 
-def fonctionnalite_9(return_matrice, nom_repertoire_nettoye):
+def fonctionnalite_9(return_matrice, nom_repertoire_nettoye):  # Fonctionnalité n°9
     print("Les mots évoqués par tous les présidents (sauf les mots dits \"non importants\") sont :")
     for mot in fct_mat.mots_tous_presidents(return_matrice, nom_repertoire_nettoye):
         print("- " + mot)
 
 
+# Fonctionnalité n°10 : Affichage de la matrice TF-IDF sous forme de tableau
 def fonctionnalite_10(return_matrice_tf_idf):
     noms_fichiers, liste_mots, matrice = return_matrice_tf_idf
 
+    # Fonction interne permettant de retourner le mot le plus long d'une liste de mots
     def taille_mot_plus_long(liste_mots):
         taille_max = 0
         for mot in liste_mots:
@@ -122,23 +130,29 @@ def fonctionnalite_10(return_matrice_tf_idf):
 
         return taille_max
 
+    # Fonction interne permettant de compléter une chaine avec des espaces en fonction d'une taille max
     def ajuster_espaces(chaine, taille_max):
         taille_restante = taille_max + 1 - len(chaine)
         return chaine + taille_restante * " "
 
+    # Récupération des termes les plus long pour adapter le tableau
     noms_fichiers_plus_long = taille_mot_plus_long(noms_fichiers)
     mot_plus_long = taille_mot_plus_long(liste_mots)
 
+    # Affichage de x * " " pour correspondre à la taille du plus grand mot
     print((mot_plus_long + len("|  |")) * " ", end="")
     for _ in range(len(noms_fichiers) - 1):
+        # Puis x * "_" en fonctione des bordures, espaces et tailles des noms de fichiers
         print((noms_fichiers_plus_long + len("  | ")) * "_", end="")
     print((noms_fichiers_plus_long + len("  |")) * "_")
+
+    # Deuxième ligne; premier espace correspondant à la taille du plus grand mot
     print((mot_plus_long + len("|  ")) * " ", end="")
-    for nom_fichier in noms_fichiers:
+    for nom_fichier in noms_fichiers:  # Affichage des noms des fichiers avec bordures
         print("|  " + ajuster_espaces(nom_fichier, noms_fichiers_plus_long), end="")
     print("|")
 
-    def fermer_tableau():
+    def fermer_tableau():  # Fonction interne permettant de fermer le tableau à chaque nouvelle ligne
         print((mot_plus_long + len("  ")) * "_", end="")
         for _ in range(len(noms_fichiers)):
             print("|___" + noms_fichiers_plus_long * "_", end="")
@@ -147,6 +161,7 @@ def fonctionnalite_10(return_matrice_tf_idf):
     print("_", end="")
     fermer_tableau()
 
+    # On étend le tableau en affichant tous les mots puis en le fermant grâce à la fonction "fermer_tableau"
     for indice_mot in range(len(liste_mots)):
         print("| " + ajuster_espaces(liste_mots[indice_mot], mot_plus_long), end="")
         for indice_fichier in range(len(noms_fichiers)):
@@ -157,15 +172,19 @@ def fonctionnalite_10(return_matrice_tf_idf):
 
 
 def main():
+    # Initialisations des constantes
     NOM_REPERTOIRE_DISCOURS = "speeches"
     NOM_REPERTOIRE_NETTOYE = "cleaned"
 
     NOM_FICHIER_PRESIDENTS = "presidents.txt"  # Noms et prénoms stockés dans ce fichier
 
+
     noms_presidents = fct_mat.recup_noms_presidents(NOM_REPERTOIRE_DISCOURS)
 
+    # Nettoyage des fichiers (suppression des caractères spéciaux, etc.)
     fct_mat.nettoyage_complet_fichiers(NOM_REPERTOIRE_DISCOURS, NOM_REPERTOIRE_NETTOYE)
 
+    # On récupère le tuple retourné par la fct "creation_matrice" (Liste des fichiers, liste des mots et la matrice)
     return_matrice_tf_idf = fct_mat.creation_matrice(NOM_REPERTOIRE_NETTOYE)
 
     print("\nBienvenue sur le ChatBot de Clément PINTO RIBEIRO et de Ismaël RADOUANE.\n")
@@ -187,12 +206,14 @@ def main():
               "Ou entrez \"q\" pour quitter\n")
 
         reponse_utilisateur = input("Entrez le numéro choisi : ")
+        # Tant que l'utilisateur ne répond pas '1', '2', '3', '4', '5', '6', '7', '8', '9', '10' ou 'q'
         while reponse_utilisateur not in [str(nombre) for nombre in range(1, 11)] + ["q"]:
             print("\nErreur : Veuillez entrer un nombre entier entre 1 et 10, ou \"q\" pour quitter.\n")
             reponse_utilisateur = input("Entrez le numéro choisi : ")
 
         print()
 
+        # En fonction de la réponse de l'utilisateur, on exécute la fonctionnalité associée
         if reponse_utilisateur == "1":
             fonctionnalite_1()
 
@@ -224,9 +245,10 @@ def main():
             fonctionnalite_10(return_matrice_tf_idf)
 
         elif reponse_utilisateur == "q":
-            demander_numero = False
+            demander_numero = False  # La boucle "while" s'arrête donc le programme également
 
         print()
 
 
-main()
+if __name__ == "__main__":  # Exécution du programme principal (fonction "main")
+    main()
